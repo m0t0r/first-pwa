0a446b90a971f51404f977f4dc7ddd88d66ee465
Vitaly Tkachenko <vitaly.tkachenko@jobsity.com>
2017-06-27 20:52:08 -0500

    feat: setup app shell

--- run.sh
+++ run.sh
@@ -5,6 +5,12 @@ set -x
 # Production build
 ng build --prod
 
+# Generate a new index.html with an app shell
+node_modules/.bin/ngu-app-shell --module src/app/app.module.ts \
+                                --insert-module src/app/loading/loading.module.ts \
+                                --url /loading \
+                                --out dist/index.html
+
 # Generate a SW manifest from out app
 node_modules/.bin/ngu-sw-manifest --module src/app/app-routing.module.ts \
                                   --out dist/ngsw-manifest.json
---
+++ src/app/loading/loading.module.ts
@@ -0,0 +1,21 @@
+import { NgModule, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-loading',
+  template: '<h3>Loading...</h3>'
+})
+
+export class LoadingComponent { }
+
+@NgModule({
+  declarations: [LoadingComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {path: 'loading', component: LoadingComponent}
+    ])
+  ]
+})
+export class LoadingModule { }
